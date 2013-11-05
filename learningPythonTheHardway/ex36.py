a8d0582f51f25dfa3d850393bb4ab586578cf4ea
gaudard <gaudard@gmail.com>
2013-11-05 15:12:20 -0500

    still working on this...

--- learningPythonTheHardway/ex36.py
+++ learningPythonTheHardway/ex36.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python -tt
 #################################################
 # Learn Python The Hard Way
 # Exercise 36
@@ -9,7 +9,7 @@
 
 #imports required for program to run
 from sys import exit
-import random
+from random import choice
 import time
 
 
@@ -18,14 +18,14 @@ sword = 0.9
 axe = 0.6
 mace = 0.3
 
-# Variables - loot/modifiers
-weaponSharping = 1.1
+# Variables - loot
 gold = 0
+weaponSharpening = 0
 resurrectionPotion = 0
 
 ''' cool banner function'''
 def banner():
-	print ''' \n
+  print ''' \n
 _________          _______ 
 \__   __/|\     /|(  ____ \
    ) (   | )   ( || (    \/
@@ -47,12 +47,31 @@ _________          _______
 
 ''' ogre attack function, with random chance of surival based on weapon'''
 def ogreAttack():
-	print 'ogre attack'
-	#### isn't returning to the calling function
+  chance1 = choice(range(0,3))
+  if chance1 == 1:
+    death()
+  else:
+    print "you didn't die."
+    corpseLooter()
+	
 
 ''' corpse looting function, with random chance of random loot'''
 def corpseLooter():
-	print 'body looted'
+  chance2 = choice(range(0, 3))
+  if chance2 == 0:
+    global weaponSharping
+    weaponSharping += 1.1
+    print "you can now sharpen your weapon"
+  elif chance2 == 1:
+    global gold
+    gold += 10
+    print "you have found 10 gold"
+  else:
+    global resurrectionPotion
+    resurrectionPotion += 0
+    print "you have found one resurrectionPotion"
+	
+
 	
 ''' death function'''
 def death():
@@ -74,39 +93,39 @@ def crawling():
 	else:
 		pass
 	
-	next0 = random.choice(range(0,2))
+	next0 = choice(range(0,2))
 	if next0 == 0:
 		ogreAttack()
 	else:
 		print "you continue on about your journey"
-	
-	
-	
-	
+		
+	print "still in the crawling function"
+
 	
 ''' main function '''
 def main():
-	banner()
-		
-	play = raw_input("\nWould you like to play the game?\n 'y' or 'n' > ")
-	if play == 'y':
-		print 'entering the game'
-	else:
-		death()
+  banner()
 	
-	print 'You have a choice'
-	print '1) Crawl into the small cave entrance'
-	print '2) Walk in to the cave entrance with the thick steel gate'
+  play = raw_input("\nWould you like to play the game?\n 'y' or 'n' > ")
+  if play == 'y':
+    print 'entering the game'
+  else:
+    death()
+	  
+  print 'You have a choice'
+  print '1) Crawl into the small cave entrance'
+  print '2) Walk in to the cave entrance with the thick steel gate'
 	
-	choice0 = raw_input('\n > ')
-	if choice0 == "1":
-		crawling()
+  choice0 = raw_input('\n > ')
+  if choice0 == "1":
+    crawling()
 
-	
+  # ***needs work returning variable from functions***
+  print 'You find yourself in the center of the earth'
+  print "loot gold: " + str(gold)
+  print "loot weapon sharpening: " + str(weaponSharpening)
+  print "loot resurrection potion: " + str(resurrectionPotion)
 
-
-
-	print 'You find yourself in the center of the earth'
 
 if __name__ == '__main__': #boilerplate to call main function
   main()
