2d558f989f18c3544e1650793c5d70554d87b403
gaudard <gaudard@gmail.com>
2013-11-14 16:55:31 -0500

    starting on ex41

---
+++ learningPythonTheHardway/ex41.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python -tt
+#################################################
+# Learn Python The Hard Way
+# Exercise 41
+# http://learnpythonthehardway.org/book/ex41.html
+# @jgaudard
+##################################################
+
